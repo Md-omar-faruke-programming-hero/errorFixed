2c57de4f1d40c9d675d6d1a5f06ef2b4b637d81d
Md-omar-faruke-programming-hero <farukeomar@gmail.com>
2021-09-13 14:59:16 +0600

    added bonous part and footer

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@
 const loadProducts = () => {
 
 
-  const url = `https://fakestoreapi.com/products`;  
+   
   // const url=`../js/data.json`; //if api server down use this manually
+
+  const url = `https://fakestoreapi.com/products`; 
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
